08089d48bab6d3d4cc40e588b2c88c59d8b0b91c
sepriano <sepriano007@gmail.com>
2022-03-14 02:44:38 +0700

    Repos

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Results = (props) => {
+    const {repos} = props;
+    console.log('Repos is: ', repos)
+    return (
+        <ul>
+        </ul>
+    );
+};
+
+export default Results;
